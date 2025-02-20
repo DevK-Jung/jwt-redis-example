db7fdee3d65a7eefe76c9aa04db98e1a8a2c79db
김정현 <wjdgus0117@ccmedia.co.kr>
2025-02-20 16:10:03 +0900

    [Feat]: mysql, jpa 설정 수정

---
+++ src/main/java/com/example/redisjwtexample/user/entity/UserEntity.java
@@ -0,0 +1,26 @@
+package com.example.redisjwtexample.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
+
+
