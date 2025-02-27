1a26f8f901105e49ffb90c4cfa62e680fa0754a1
김정현 <wjdgus0117@ccmedia.co.kr>
2025-02-27 09:34:16 +0900

    [Refactor]: CookieUtils.java Refactoring

--- src/main/java/com/example/redisjwtexample/utils/CookieUtils.java
+++ src/main/java/com/example/redisjwtexample/utils/CookieUtils.java
@@ -8,45 +8,66 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class CookieUtils {
 
-    public void setRefreshTokenCookie(String name,
-                                      String refreshToken,
-                                      long maxAgeSeconds,
-                                      boolean isSecure) {
+    /**
+     * 쿠키 세팅
+     *
+     * @param key           쿠키 명
+     * @param value         쿠키 값
+     * @param path          접근 가능 경로
+     * @param maxAgeSeconds 만료 시간 (초 단위)
+     * @param isSecure      HTTPS 에서만 전송 가능 여부
+     */
+    public void setCookie(String key,
+                          String value,
+                          String path,
+                          long maxAgeSeconds,
+                          boolean isSecure) {
 
         HttpServletResponse response = ServletUtils.getServletResponse();
 
-        Cookie cookie = new Cookie(name, refreshToken);
-        cookie.setHttpOnly(true); // JavaScript에서 접근 불가 (XSS 방어)
+        Cookie cookie = new Cookie(key, value);
+        cookie.setHttpOnly(isSecure); // JavaScript에서 접근 불가 (XSS 방어)
         cookie.setSecure(isSecure); // HTTPS에서만 전송 (보안 강화)
-        cookie.setPath("/"); // 모든 경로에서 접근 가능
+        cookie.setPath(path); // 모든 경로에서 접근 가능
         cookie.setMaxAge((int) maxAgeSeconds); // 만료 시간 설정 (초 단위)
 
         response.addCookie(cookie);
     }
 
-    public String getCookie(String name) {
+    /**
+     * 쿠키 조회
+     *
+     * @param key 쿠키 명
+     * @return value 쿠키 값
+     */
+    public String getCookie(String key) {
 
         HttpServletRequest request = ServletUtils.getServletRequest();
 
         Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (name.equals(cookie.getName())) {
-                    return cookie.getValue();
-                }
+
+        if (cookies == null) return null;
+
+        for (Cookie cookie : cookies) {
+            if (key.equals(cookie.getName())) {
+                return cookie.getValue();
             }
         }
+
         return null;
     }
 
     /**
-     * Refresh Token 쿠키 삭제 (로그아웃 시 사용)
+     * 쿠키 삭제
+     *
+     * @param key 쿠키 명
      */
-    public void deleteCookie(String name) {
+    public void deleteCookie(String key) {
 
         HttpServletResponse response = ServletUtils.getServletResponse();
 
-        Cookie cookie = new Cookie(name, "");
+        Cookie cookie = new Cookie(key, "");
+
         cookie.setHttpOnly(true);
         cookie.setSecure(true);
         cookie.setPath("/");
